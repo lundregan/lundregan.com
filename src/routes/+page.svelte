b5112935b84d50e6f6a9d7c3992ebabdab6d6ab7
Lundregan <lundregan.ethan@gmail.com>
2023-10-17 20:19:29 +0100

    content fleshing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,9 @@
 	import LogoOutwardbound from '$lib/img/logo/outwardbound.svg?raw'
 	import LogoHubpay from '$lib/img/logo/hubpay.svg?raw'
 
+	// Components
+	import Collabs from '$lib/collabs.svelte'
+
 	const stats = [
 		{
 			value: '30+',
@@ -26,18 +29,18 @@
 	const showcaseSkills = [
 		{
 			icon: IoMdRocket,
-			heading: 'Frontend',
-			text: 'Crafting visually engaging and user-centric interfaces, I specialize in creating responsive and intuitive web designs.'
+			heading: 'Frontend Excellence',
+			text: 'Combining art with functionality, I craft visually engaging and user-centric interfaces.'
 		},
 		{
 			icon: FaServer,
-			heading: 'Backend',
-			text: 'Engineering the backbone of your web applications, I deliver robust and scalable solutions to power your digital platforms.'
+			heading: 'Backend Development',
+			text: 'Engineering resilient, scalable, and efficient digital platforms that run smoothly and reliably.'
 		},
 		{
 			icon: GoLink,
-			heading: 'Full Stack Integration',
-			text: 'Engineering the backbone of your web applications, I deliver robust and scalable solutions to power your digital platforms.'
+			heading: 'Full Stack Synergy',
+			text: 'Marrying frontend artistry with backend reliability, delivering stellar UX/UI with optimal performance.'
 		}
 	]
 
@@ -58,40 +61,73 @@
 	]
 </script>
 
-<main class="h-full w-full flex flex-col items-center container mx-auto pb-8">
-	<section class="flex flex-col lg:flex-row-reverse gap-16 w-full px-4 justify-between items-center py-80">
+<main class="h-full w-full flex flex-col items-center container mx-auto pb-8 px-4 md:pb-40">
+	<section class="flex flex-row-reverse gap-16 w-full px-4 justify-between items-center min-h-[100svh]">
 			<Avatar
 				src="https://i.ibb.co/RQMCdT4/public-profile-square-styled-blue.png"
-				width="w-0 lg:w-80 w-0 lg:h-80"
+				width="w-0 lg:w-80 w-0 lg:h-80 hidden lg:block"
 				rounded="rounded-full"
 			/>	
 		
-			<div class="max-w-xl">
-				<h1 class="text-primary font-bold h1">Hey, I'm Ethan👋</h1>
-				<p class="text-2xl dark:text-gray-400 font-medium text-gray-700 mt-8">A dynamic and passionate Developer with 3 years of experience in crafting seamless, responsive, and user-friendly web applications. Always eager to learn and take on interesting new challenges. </p>
+			<div class="lg:max-w-2xl text-center md:text-left">
+				<h1 class="text-primary font-bold h1 font-serif">Hey, I'm Ethan👋</h1>
+				<p class="text-3xl dark:text-gray-300 font-medium text-gray-700 mt-8 font-league">A dynamic and passionate Developer with 3 years of experience in crafting seamless, responsive, and user-friendly web applications. Always eager to <a class="anchor" href="#about">learn more</a> and take on interesting new challenges. </p>
 			</div>
 	</section>
 
 	<section class="w-full">
-		<h2 class="h3 text-gray-400 text-center">
+		<h2 class="h3 dark:text-gray-400 px-8 text-left sm:text-center font-serif">
 			Proudly worked with:
 		</h2>
-		<div class="py-8 flex flex-col md:flex-row items-center gap-16 text-secondary w-full justify-between">
+		<div class="py-8 flex flex-col sm:flex-row items-center mt-16 sm:mt-0 gap-32 sm:gap-16 text-secondary w-full justify-between">
 			{#each proudToWorkWithList as company}
-				<div class="{company.logoClass}">{@html company.logo}</div>
+				<div class="{company.logoClass} text-gray-700 dark:text-gray-200">{@html company.logo}</div>
 			{/each}
 		</div>
 	</section>
 
-	<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-40">
-		{#each showcaseSkills as skill}
-			<div class="card p-4 flex flex-col gap-4 items-center">
-				<div class="w-16 mx-auto text-primary-700">
-					<svelte:component this={skill.icon} />
-				</div>
-					<h3 class="h3 font-medium">{skill.heading}</h3>
-				<p class="font-medium dark:text-gray-100 text-gray-600 p-4 text-center">{skill.text}</p>
+	<section class="flex flex-col gap-16 pt-32" id="about">
+		<div class="text-xl font-medium">
+			<div class="relative">
+				<h1 class="h1 font-medium mb-8 font-serif">About</h1>
+				<!-- <span class="absolute left-0 top-0 -translate-y-20 pl-8 opacity-30 z-[-10] text-[200px] text-gray-800">About</span> -->
 			</div>
-		{/each}
+			<div class="flex flex-col gap-6 text-2xl font-medium font-league">
+				<p>
+					I'm a web developer from the UK and have always been passionate about
+					technology and have dedicated my career to building beautiful and functional websites.
+				</p>
+				<p>
+					I have worked with a variety of clients, creating websites for businesses, non-profits, and
+					individuals. In my free time, you can find me tinkering with new technologies, learning
+					about design principles, or staying up-to-date on industry trends.
+				</p>
+				<p>
+					<span>You can alway</span>
+					<a
+						href="mailto:contact@lundregan.com?subject=Hello Ethan - Contacted from  From Lundregan.com"
+						class="dark:text-blue-300 hover:underline font-medium">
+						Get in touch
+					</a>
+					<span>if you have any questions or want to collaborate on a project.</span>
+				</p>
+			</div>
+		</div>
+
+		<div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
+			{#each showcaseSkills as skill}
+				<div class="card variant-ringed-primary p-4 flex flex-col gap-4 items-center hover:scale-105 transition-transform">
+					<div class="w-16 mx-auto text-primary-500 py-4">
+						<svelte:component this={skill.icon} />
+					</div>
+					<h3 class="h3 font-bold text-center font-serif">{skill.heading}</h3>
+					<p class="font-medium dark:text-gray-100 text-gray-600 px-4 text-center text-xl font-league">{skill.text}</p>
+				</div>
+			{/each}
+			</div>
+	</section>
+
+	<section class="w-full mt-40">
+		<Collabs />
 	</section>
 </main>
