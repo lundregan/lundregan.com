b10e7420581ab5936385065f9ef09c89b4624b40
Lundregan <lundregan.ethan@gmail.com>
2023-10-17 20:20:36 +0100

    adjusted base tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
+		fontFamily: {
+			'sans': ['League Spartan', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+			'serif': ['Roboto', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+			'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace',],
+		},
 		extend: {},
 	},
 	plugins: [
