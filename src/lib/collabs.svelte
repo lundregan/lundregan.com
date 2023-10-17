878da93844c915115cb115603700725d8266c042
Lundregan <lundregan.ethan@gmail.com>
2023-10-17 20:46:26 +0100

    layout style

--- src/lib/collabs.svelte
+++ src/lib/collabs.svelte
@@ -42,11 +42,11 @@
 
 <div class="w-full flex flex-col gap-8">
     <h2 class="h1 font-serif">Collaborations</h2>
-    <div class="logo-cloud grid-cols-1 sm:grid-cols-2 gap-x-2 gap-y-2 md:gap-y-1 md:gap-4 overflow-visible {logoCloud.length % 2 == 0 ? 'lg:!grid-cols-4' : 'lg:!grid-cols-3'}" >
+    <div class="logo-cloud grid-cols-1 md:grid-cols-2 gap-x-2 gap-2 md:gap-4 overflow-visible {logoCloud.length % 2 == 0 ? 'lg:!grid-cols-4' : 'lg:!grid-cols-3'}" >
         {#each logoCloud as cloud}
-            <a class="logo-item group variant-glass-primary dark:variant-glass-secondary p-4 hover:z-10" href={cloud.url} target="_blank">
+            <a class="logo-item group variant-filled-primary dark:variant-glass-secondary p-4 hover:z-10" href={cloud.url} target="_blank">
                 <!-- <span>(icon)</span> -->
-                <span class="text-2xl group-hover:font-medium font-medium dark:text-white">{cloud.name}</span>
+                <span class="text-2xl group-hover:font-medium font-medium text-white">{cloud.name}</span>
             </a>
         {/each}
     </div>
