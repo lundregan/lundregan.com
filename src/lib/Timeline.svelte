41df0dc89d9c43f53a1ce3c1db3c302b5c8b86c3
Lundregan <lundregan.ethan@gmail.com>
2023-11-12 20:26:39 +0000

    Update Timeline component and TimelineItem styling

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -6,27 +6,30 @@
             heading: 'Honcho',
             subheading: 'Web Developer',
             text: 'Developing custom and impactful web solutions, uniquely tailored to meet the distinct needs for a plethora of clientele',
-            link: 'https://honcho.agency/'
+            link: 'https://honcho.agency/',
+            year: '2022'
         },
         {
             heading: 'Smart Container Co',
             subheading: 'Software Engineer',
             text: 'Developing real-time IoT End-User Platform, includeding a simulation of real-time keg data and the accompanying analytical dashboards for early investors.',
-            link: 'https://www.smartcontainer.co.uk/'
+            link: 'https://www.smartcontainer.co.uk/',
+            year: '2021'
         },
         {
             heading: 'Digital Virtue',
             subheading: 'Junior Web Developer',
             text: 'As an ambitious student learning web development, I had the opportunity to collaborate with a dynamic team of three developers, delivering high-quality websites for clients across Cardiff',
-            link: 'https://www.digitalvirtue.com'
+            link: 'https://www.digitalvirtue.com',
+            year: '2016'
         }
     ]
 </script>
 
 <!-- component -->
-<div class="container mx-auto w-full h-full">
+<div class="container mx-auto w-full h-full my-16">
     <div class="relative wrap overflow-hidden p-10 h-full">
-        <div class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border" style="left: 50%"></div>
+        <div class="border-2-2 absolute border-opacity-20 border-surface-600 h-full border" style="left: 50%"></div>
         
         {#each timelineItemsList as timelineItem, index}
             <TimelineItem data={timelineItem} index={index} />
