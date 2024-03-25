cb937f8a781cba84ae62ec794f8c877763f6d517
Lundregan <lundregan.ethan@gmail.com>
2024-03-25 20:30:40 +0000

    updated copy

--- src/lib/sections/Projects.svelte
+++ src/lib/sections/Projects.svelte
@@ -11,34 +11,34 @@
     const featuredProjects = [
         {
             title: 'Ravencards',
-            description: 'Landing page design for SRS app, demonstrating visual hierarchy, persuasive copy, and UI principles.',
+            description: `Landing page design for SRS app, demonstrating visual hierarchy, persuasive copy, and UI principles.`,
             image: imgRavencards,
             link: 'https://www.google.com'
         }
     ]
 
-    const projects = [
+    const honchoProjects = [
         {
             title: 'Outwardbound Trust',
-            description: 'This is a project description',
+            description: `We Enhanced the user experience for Outward Bound's website, including visual updates and streamlining the donation process. I also contributed to updates and fixes to the backend donation system.`,
             image: imgOutwardbound,
             link: 'https://www.google.com'
         },
         {
             title: 'envair',
-            description: 'This is a project description',
+            description: `Visual refresh for Envair Technology's website, aligning with their cutting-edge brand and enhancing user experience.`,
             image: imgEnvair,
             link: 'https://www.google.com'
         },
         {
             title: 'Honcho',
-            description: 'This is a project description',
+            description: `Website refresh for Honcho's lead-generating website, a key tool for attracting new clients. Ensuring seamless functionality and a design that highlights the agency's strengths.`,
             image: imgHoncho,
             link: imgHoncho
         },
         {
             title: 'SMG',
-            description: 'This is a project description',
+            description: `We collaborated with the Santa Marta Group to modernize their website, ensuring alignment with their mission and increased online efficacy.`,
             image: imgSmg,
             link: 'https://www.google.com'
         },
@@ -50,7 +50,7 @@
         },
         {
             title: 'CPI',
-            description: 'This is a project description',
+            description: `Ongoing visual enhancements for the Centre for Process Innovation (CPI) website, ensuring a dynamic and engaging user experience.`,
             image: imgCpi,
             link: 'https://www.google.com'
         }
@@ -85,18 +85,18 @@
             <p class="mb-4 text-xl">Websites worked on whilst at <a href="https://honcho.agency" target="_blank" rel="">Honcho</a></p>
 
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {#each projects as project}
+                {#each honchoProjects as honchoProject}
                     <div class="card variant-glass-primary rounded-xl overflow-hidden block">
                         <header class="card-header p-px"><figure class="relative">
                             <div class="absolute top-0 -translate-x-1/2 left-1/2 bg-primary-500 px-4 pt-1 rounded-b">Honcho</div>
-                            <img src={project.image} alt={project.title} class="scale-105 w-full h-48 object-cover object-top">
+                            <img src={honchoProject.image} alt={honchoProject.title} class="scale-105 w-full h-48 object-cover object-top">
                         </figure></header>
                         <section class="p-4">
-                            <h3 class="font-semibold text-lg">{project.title}</h3>
-                            <p class=" mt-2">{project.description}</p>
+                            <h3 class="font-semibold text-lg">{honchoProject.title}</h3>
+                            <p class=" mt-2">{honchoProject.description}</p>
                         </section>
                         <footer class="card-footer">
-                            <a href={project.link} class="btn btn-sm variant-filled-primary text-blue-500 mt-2">View Project</a>
+                            <a href={honchoProject.link} class="btn btn-sm variant-filled-primary text-blue-500 mt-2">View Project</a>
                         </footer>
                     </div>
                 {/each}
