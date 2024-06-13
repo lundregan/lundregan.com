fee4aa1295729cd7bd5248789c9b4307fe56e8b6
Lundregan <lundregan.ethan@gmail.com>
2024-06-13 18:07:09 +0100

    moved affliated companies to section component

---
+++ src/lib/sections/Affiliates.svelte
@@ -0,0 +1,33 @@
+<script>
+    // Proud to work with logos
+	import LogoHoncho from '$lib/img/logo/honcho.svg?raw'
+	import LogoOutwardbound from '$lib/img/logo/outwardbound.svg?raw'
+	import LogoEckoh from '$lib/img/logo/eckoh.svg?raw'
+    
+    const proudToWorkWithList = [
+		{
+			logo: LogoOutwardbound,
+			alt: 'Outwardbound Company Logo'
+		},
+		{
+			logo: LogoHoncho,
+			alt: 'Honcho Company Logo'
+		},
+		{
+			logo: LogoEckoh,
+			alt: 'Eckoh Company Logo',
+			logoClass: 'pb-3'
+		}
+	]
+</script>
+
+<section class="w-full">
+    <h2 class="h3 dark:text-gray-400 px-8 text-left sm:text-center font-serif">
+        Proudly worked with:
+    </h2>
+    <div class="py-8 flex flex-col sm:flex-row items-center mt-16 sm:mt-0 gap-16 text-secondary w-full justify-between">
+        {#each proudToWorkWithList as company}
+            <div class="{company.logoClass} text-gray-700 dark:text-gray-200 h-32">{@html company.logo}</div>
+        {/each}
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@
 	import Skills from '$lib/sections/Skills.svelte'
 	import Projects from '$lib/sections/Projects.svelte';
 	import About from '$lib/sections/About.svelte';
+	import Affiliates from '$lib/sections/Affiliates.svelte';
 
 	const stats = [
 		{
@@ -71,16 +72,7 @@
 <main class="h-full w-full flex flex-col gap-32 items-center container mx-auto pb-8 px-4 md:pb-40">
 	<Hero />
 
-	<section class="w-full">
-		<h2 class="h3 dark:text-gray-400 px-8 text-left sm:text-center font-serif">
-			Proudly worked with:
-		</h2>
-		<div class="py-8 flex flex-col sm:flex-row items-center mt-16 sm:mt-0 gap-16 text-secondary w-full justify-between">
-			{#each proudToWorkWithList as company}
-				<div class="{company.logoClass} text-gray-700 dark:text-gray-200 h-32">{@html company.logo}</div>
-			{/each}
-		</div>
-	</section>
+	<Affiliates />
 
 	<Projects />
 
