8bfc1d70224d80ceb0225df6a33bff748d903c48
Lundregan <lundregan.ethan@gmail.com>
2025-03-04 18:15:11 +0000

    reverted to red

--- src/lib/Heading.svelte
+++ src/lib/Heading.svelte
@@ -2,6 +2,6 @@
 	export let text = '';
 </script>
 
-<div class="flex flex-col mb-4">
-    <h2 class="text-3xl text-gray-200">{text}</h2>
+<div class="mb-4 flex flex-col">
+	<h2 class=" text-red-500">{text}</h2>
 </div>
