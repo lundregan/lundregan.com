56cdf03cec6767f1a8e01742cb80021814de02bb
Lundregan <lundregan.ethan@gmail.com>
2025-03-04 18:24:36 +0000

    added avaliable badge

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -13,20 +13,13 @@
 		</a>
 	</div>
 	<div class="flex-none">
-		<button class="btn btn-square btn-ghost">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="inline-block h-5 w-5 stroke-current"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-				></path>
-			</svg>
-		</button>
+		<div class="badge bg-zinc-950">	
+			<div class="inline-grid *:[grid-area:1/1]">
+				<div class="status status-success animate-ping scale-125"></div>
+				<div class="status status-success scale-125"></div>
+			</div>
+	
+			<span class="whitespace-nowrap text-xs">Available</span> 
+		</div>
 	</div>
 </div>
